b98a363962fb32d438b643006bc8dcfd5d074886
Elias Consalvo <econs013@fiu.edu>
2022-07-18 20:20:01 -0400

    added new URL pathname for heroku

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,11 @@ app.use(express.urlencoded({extended: true}))
 
 app.use('/auth', authRoute);
 app.use('/account', authenticateJWT, accountRoute)
-app.use('/', (req, res)=>{
-    res.send('Origin');
+
+app.use(express.static(new URL('./bank-client/build', import.meta.url).pathname));
+
+app.get('*', (req, res)=>{
+    res.sendFile(new URL('./bank-client/build/index.html', import.meta.url).pathname);
 });
 
 app.listen(PORT, ()=>{
